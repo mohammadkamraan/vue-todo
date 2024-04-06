17baa87ef574b75c0d448be24ee5c1f48a9bb9cc
mohammadkamraan <kamyzenon@gmail.com>
2024-04-06 13:16:01 +0330

    Created single todo get handler

--- src/API/API-handler.ts
+++
@@ -1,5 +0,0 @@
-import type { Methods } from '@/Constants/API-mehods'
-
-class RequestOptions {
-  private _method: Methods
-}
--- src/API/Todos.ts
+++ src/API/Todos.ts
@@ -1,20 +1,48 @@
 import { endPoints, endPointUrl } from '@/Constants/EndPoint'
 import { Todo } from '@/Models/Todo'
+import { RequestHandler } from './handlers/RequestHandler'
+import { Methods } from '../Constants/API-mehods'
+import { RequestOptions } from './handlers/RequestOptions'
 
 export class TodoApi {
   private static todoApiInstance: TodoApi | null = null
 
   private constructor() {}
 
-  protected mapDynamicTodosToTodosModel(unTypedTodos: { [key: string]: any }[]): Todo[] {
+  private mapDynamicTodosToTodosModel(unTypedTodos: { [key: string]: any }[]): Todo[] {
     return unTypedTodos.map((todo) => new Todo(todo.userId, todo.id, todo.title, todo.completed))
   }
 
-  public async getAllTodos(): Promise<Todo[]> {
-    const response = await fetch(endPointUrl + endPoints.AllTodos)
-    const data = await response.json()
-    const mapedTodos = this.mapDynamicTodosToTodosModel(data)
-    return mapedTodos
+  private mapUntypedTodoToModel(unTypedTodo: { [key: string]: any }) {
+    return new Todo(unTypedTodo.userId, unTypedTodo.id, unTypedTodo.title, unTypedTodo.completed)
+  }
+
+  public async getAllTodos(): Promise<[Todo[] | null, Error]> {
+    let mappedTodos: Todo[] | null = null
+    const requestHandler = new RequestHandler()
+    const [data, error] = await requestHandler.sendRequest(
+      endPointUrl + endPoints.AllTodos,
+      new RequestOptions(Methods.Get)
+    )
+
+    if (data) mappedTodos = this.mapDynamicTodosToTodosModel(data)
+
+    return [mappedTodos, error]
+  }
+
+  public async getTodo(todoId: number): Promise<[Todo | null, Error]> {
+    let mappedTodo: Todo | null = null
+
+    const requestHandler = new RequestHandler()
+
+    const [data, error] = await requestHandler.sendRequest(
+      endPointUrl + endPoints.AllTodos + todoId,
+      new RequestOptions(Methods.Get)
+    )
+
+    if (data) mappedTodo = this.mapUntypedTodoToModel(data)
+
+    return [mappedTodo, error]
   }
 
   public static GetInstance(): TodoApi {
